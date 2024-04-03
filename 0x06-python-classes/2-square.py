5e747cd593cf19de4cc87c39e103e838f25e11d6
KhurasMak <makhurakgotlello@gmail.com>
2024-04-03 16:59:00 +0200

    write a class instance that defines a class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,9 +6,9 @@ class Square:
     """empty class"""
     def __init__(self, size=0):
         """initialize sqaure attributes"""
+        if isinstance(size, int):
+            self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size <= -1:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size

0a80e0aa5cc042a4c7564977d5dea99f6a97b7f4
KhurasMak <makhurakgotlello@gmail.com>
2024-04-03 16:40:37 +0200

    write a class instance that defines a class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     def __init__(self, size=0):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size <= -1:
             raise ValueError("size must be >= 0")
